01184941c568d713af5ebced4710ca03ff5f9dab
zyril1A <perezyrelle.07@gmail.com>
2026-08-13 11:27:11 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,6 @@
+# escape sequence
+# a way to modify inline string
+
+phase_mask = "kumain ka na ba po \r\\\"kumain ngayon ba?\"\\ \n \t\t\t ah\b\b gegeggegegege"
+
+print(phase_mask)
---
+++ code_challenge1.py
@@ -0,0 +1,13 @@
+# code challenge
+
+print("\t\t\t\t\t\t\t\t\t\t\t\t\t*"
+     "\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*"
+     "\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*"
+     "\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*"
+     "\n\t\t\t\t\t\t\t\t\t*\t\t\tMy Name is ...\t\t\t\t*"
+     "\n\t\t\t\t\t\t\t\t*\t\t\t\t\"Zyrelle D. Perez\"\t\t\t\t*"
+     "\n\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*"
+     "\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*"
+     "\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*"
+     "\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*"
+     "\n\t\t\t\t\t\t\t\t\t\t\t\t\t*") 
