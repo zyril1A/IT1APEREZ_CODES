d6c747d9d987bad0e4e50aa4f5fd07b90bff45a1
zyril1A <perezyrelle.07@gmail.com>
2026-08-13 10:32:53 +0800

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,4 @@
+# This is my first program
+# as an IT student
+
+print("Hello World, Hello Philippines")
---
+++ activity3.py
@@ -0,0 +1,36 @@
+#integer variable
+
+age = 19
+num_of_students = 45
+siblings = 0
+quantity = 10
+
+#float variable
+
+price = 33.3
+temp = 38.9
+weight = 60.8
+height = 170.77
+
+#string variable
+
+name = "Zyrelle D. Perez"
+school = "DLL"
+course = "BSIT"
+year_level = "First Year"
+unit = "kg"
+
+#boolean variable 
+
+isMarried = False
+isRegular = False 
+isPresent = True
+isEnrolled = True
+
+# print 
+
+print("Hi, my name is",name)
+print("I'm ",age, "years old")
+print("I weigh", weight, unit)
+print("Am I a regular student?", isRegular)
+print(temp, "- High Temp")
