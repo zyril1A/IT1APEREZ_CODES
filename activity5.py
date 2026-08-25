ebcc17dac0dfca444e4689c8ac482e74b3c1fecd
zyril1A <perezyrelle.07@gmail.com>
2026-08-25 10:57:35 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,30 @@
+# operators
+# + - Addition
+# - - Substraction
+# * - Multiplication
+# ** - Exponentation
+# % - Modulus
+# / - Division
+# // - Floor Division
+
+a = 23
+b = 11
+
+sum = a + b
+difference = a - b
+product = a * b
+power = a**b
+modulo = a%b
+quotient = a/b
+floor_quotient = a//b
+
+print("The sum of", a, "and", b, "is", sum, "\b.")
+print("The difference of", a, "and", b, "is", difference, "\b.")
+print("The product of", a, "and", b, "is", product, "\b.")
+print(a, " to the power of", b, "is", power, "\b.")
+print("The modulo of", a, "and", b, "is", modulo, "\b.")
+print("The quotient of", a, "and", b, "is", quotient, "\b.")
+print("The floor quotient of", a, "and", b, "is", floor_quotient, "\b.")
+
+
+
