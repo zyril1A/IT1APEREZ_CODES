39873577495ab9ad623312ba88920388768bad4c
zyril1A <perezyrelle.07@gmail.com>
2026-08-25 11:23:00 +0800

    Add files via upload

---
+++ code_challenge2.py
@@ -0,0 +1,26 @@
+# code challenge 2 
+
+money = 1540
+
+# computation
+a = 1000
+b = 500
+c = 200
+d = 100
+e = 50
+f = 20
+g = 10
+h = 5
+i = 1
+
+# print
+print("Money to deposit:", money)
+print("\n1000 -", money//a)
+print("500 -", (money%a)//b)
+print("200 -", (money%b)//c)
+print("100 -", (money%c)//d)
+print("50 -", (money%d)//e)
+print("20 -",(money%e)//f)
+print("10 -", (money%f)//g)
+print("5 -", (money%g)//h)
+print("1 -", (money%h)//i)
