6da6f5716378fe3d079a6c95d1d20ed16c447670
zyril1A <perezyrelle.07@gmail.com>
2026-09-03 10:41:49 +0800

    Add files via upload

---
+++ activity6.py
@@ -0,0 +1,9 @@
+# int(), or eval(), float()
+
+n_1 = eval(input("Num 1 = "))
+n_2 = eval(input("Num 2 = "))
+
+sum = n_1 + n_2
+
+print("The sum of", n_1, "and", n_2, "is", sum)
+print(type(sum))
---
+++ activity7.py
@@ -0,0 +1,20 @@
+# assignment operators
+
+total = 0
+
+n1 = eval(input("Input a number: "))
+total += n1
+
+n2 = eval(input("Input a number: "))
+total += n2
+
+n3 = eval(input("Input a number: "))
+total += n3
+
+n4 = eval(input("Input a number: "))
+total += n4
+
+n5 = eval(input("Input a number: "))
+total += n5
+
+print("Total:", total)
