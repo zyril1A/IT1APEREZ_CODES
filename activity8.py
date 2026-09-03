940d61f682cb96092156b248b62e01e757766feb
zyril1A <perezyrelle.07@gmail.com>
2026-09-03 10:55:23 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,23 @@
+hobbies = "" # blank string
+
+hb = input("What are your hobbies? ")
+
+hobbies += hb + ", "
+
+hb = input("What else? ")
+
+hobbies += hb + ", "
+
+hb = input("What else? ")
+
+hobbies += hb + ", "
+
+hb = input("What else? ")
+
+hobbies += hb + ", and "
+
+hb = input("What else? ")
+
+hobbies += hb + "."
+
+print("My hobbies are", hobbies)
