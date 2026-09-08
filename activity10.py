b830bf4acb12db1e1ba222df74a38b74e3673773
zyril1A <perezyrelle.07@gmail.com>
2026-09-08 10:41:25 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,11 @@
+# and or not
+
+x = 10
+y = 5
+
+username = 'User1'
+password = 'pogiako123'
+
+print(x >= y or username == 'user1' and password == 'pogiako123')
+
+print(not(x >= y or username == 'user1' and password == 'pogiako123'))
---
+++ activity9.py
@@ -0,0 +1,21 @@
+# relational operators
+
+x = 23
+y = 11
+
+user1 = "lily"
+user2 = "Lily"
+
+#1 
+print(x > y)
+#2
+print(x < y)
+#3
+print(x >= y)
+#4
+print(x <= y)
+#5
+print(x == y)
+print(user1 == user2)
+#6
+print(user1 != user2)
