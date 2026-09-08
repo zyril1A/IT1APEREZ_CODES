7066ec6ebf74f8e1cc17beb30a7df5162677ceb4
zyril1A <perezyrelle.07@gmail.com>
2026-09-08 11:02:56 +0800

    Add files via upload

---
+++ activity11.py
@@ -0,0 +1,12 @@
+# conditional statement, control structure, selection statement
+
+username = 'zyril'
+password = 'lilys'
+
+u = input("Enter Username ----> ")
+p = input("Enter Password ----> ")
+
+if username == u and password == p:
+	print("ACCESS GRANTED")
+else:
+	print("ACCESS DENIED")
